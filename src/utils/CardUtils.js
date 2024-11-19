64079244537fafb54d60059173e74e86a73b5e82
Azerus96 <gencinski1996@gmail.com>
2024-11-19 03:21:40 +0300

    Create CardUtils.js

---
+++ src/utils/CardUtils.js
@@ -0,0 +1,13 @@
+export const initializeDeck = () => {
+  const suits = ['♠', '♥', '♦', '♣'];
+  const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+  let deck = [];
+  for (let suit of suits) {
+    for (let rank of ranks) {
+      deck.push({ rank, suit });
+    }
+  }
+
+  return deck;
+};
